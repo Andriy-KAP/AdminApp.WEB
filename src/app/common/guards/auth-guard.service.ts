47e72200231cf3c866a87fc522393df2d3298691
9utell <9utell@gmail.com>
2018-05-25 14:19:45 +0200

    Redirecting if token is null or expired.

--- src/app/common/guards/auth-guard.service.ts
+++ src/app/common/guards/auth-guard.service.ts
@@ -1,15 +1,22 @@
-import { CanActivate } from "@angular/router";
+import { CanActivate, Router } from "@angular/router";
 import { JwtHelper } from "angular2-jwt/angular2-jwt";
 import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private jwtHelper: JwtHelper){
+    constructor(private jwtHelper: JwtHelper, private router: Router){
 
     }
     canActivate(route, state){
         let token = sessionStorage.getItem('auth');
-
-        return this.jwtHelper.isTokenExpired(token) ? false : true;
+        if(token != null){
+            if(this.jwtHelper.isTokenExpired(token)){
+                this.router.navigate(['login']);
+                return false;//token is expired
+            }
+            return true;//token isn`t expired
+        }
+        this.router.navigate(['login']);
+        return false;//token is null
     }
 }
